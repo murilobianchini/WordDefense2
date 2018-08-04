51f2c4c60f63edb90cb18eb440911a3c8f3c0af8
Murilo Bianchini <murilo.bianchini@gmail.com>
2018-08-04 13:03:40 -0300

    Beggining of the structure

---
+++ Assets/Scripts/Asset/AssetController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Networking;
+
+namespace Asset
+{
+    public abstract class AssetController
+    {
+
+        protected void DownloadAsset(string url, string version)
+        {
+            
+        }
+    }
+}
